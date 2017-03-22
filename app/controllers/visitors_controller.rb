c234761aee015c48bec1d2e46ce0269eeb88239c
Trungx <xtrung.net@gmail.com>
2017-03-22 13:33:51 +0700

    implement pincode check

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,24 @@
 class VisitorsController < ApplicationController
 	def index
-		@posts = Post.includes(:comments).recent
+		if check_pincode
+			@posts = Post.includes(:comments).recent	
+		else
+			render '_pincode_input'
+		end
+	end
+
+	def pincode
+		pincode = params[:pincode]
+		if user = User.find_by(pin_code: pincode)
+			session[:user_id] = user.id
+		else
+			flash[:error] = "Wrong pincode"
+		end
+		redirect_to action: 'index'
+	end
+
+	private
+	def check_pincode
+		return session[:user_id]
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'visitors#index'
+  post 'pincode', to: 'visitors#pincode'
   resources :posts
   resources :comments
 end
